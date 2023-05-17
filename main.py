bb819a091468c181924c9f39d0cc710fe34925e8
Oliver Modinger <modingeroliver@gmail.com>
2023-05-17 09:31:22 -0400

    2d commit

--- main.py
+++ main.py
@@ -4,44 +4,32 @@ import pandas as pd
 
 
 paginas= [
-    'https://n1g.cl/Home/computacion/2178-lian-li-gpu-bracket-kit-upright-black-for-lian-li-o11-dynamic-evo.html',
-    'https://n1g.cl/Home/computacion/2096-fantech-cg74-gabinete-atx-rgb-black-edition-vidrio-templado-sin-fan.html',
-    'https://n1g.cl/Home/computacion/243-gabinete-fantech-strike-cg72-rgb-atx-vidrio-templado-sin-fuente-negro.html',
-    'https://n1g.cl/Home/computacion/543-gabinete-black-5915-atx-micro-atx-mini-itx-fuente-300w-2x-usb-20-audio.html',
-    'https://n1g.cl/Home/computacion/428-gabinete-slim-clio-cl-s605-500w-micro-atx-itx-fuente-de-poder.html',
-    'https://n1g.cl/Home/computacion/2186-gabinete-black-hole-2x-200mm-argb-gamemax-atx-sin-fuente.html',
-    'https://n1g.cl/Home/computacion/893-inwin-gabinete-atx-101-tuf-gaming.html',
-    'https://n1g.cl/Home/computacion/2185-gabinete-gamer-gamemax-white-diamond-rgb-atx-m-atx.html',
-    'https://n1g.cl/Home/computacion/2187-gamemax-brufen-c3-whitepink.html',
-    'https://n1g.cl/Home/computacion/546-gabinete-itx-stratos-vidrio-templado-argb-2x-fan-120mm-high-end-vga-fuente-sfx.html',
-    'https://n1g.cl/Home/computacion/2188-gabinete-precision-white-120mm-fan-argb-e-atx-atx-matx-gamer-gamemax-controladora-sin-fuente.html',
-    'https://n1g.cl/Home/computacion/2302-gabinete-mini-itx-cougar-qbx-black-psu-atx-ultra-compact-pro-gaming-case.html',
-    'https://n1g.cl/Home/computacion/1293-msi-mpg-sekira-100r-4x-120mm-argb-gabinete-atx-e-atx-micro-atx-mini-itx.html',
-    'https://n1g.cl/Home/computacion/687-inwin-development-bp655fh300tb3-haswell-miniitx-chasis-bp655-casos-bp655fh300tb3.html',
-    'https://n1g.cl/Home/computacion/2087-lian-li-tu150-itx-silver-gabinete-de-aluminio-y-vidrio-templado-tu150wa.html',
-    'https://n1g.cl/Home/computacion/2088-lian-li-tu150-itx-black-gabinete-de-aluminio-y-vidrio-templado-tu150xa.html',
-    'https://n1g.cl/Home/computacion/544-gabinete-rackeable-4u-para-server-atx-e-atx-rackmount-incluye-rieles-y-manillas.html',
-    'https://n1g.cl/Home/computacion/1294-msi-mpg-sekira-500p-black-steel-4x-120mm-argb-gabinete-atx-e-atx-micro-atx-mini-itx.html',
-    'https://n1g.cl/Home/computacion/2303-gabinete-in-win-explorer-iw-cs-explorer-tempered-glass-abs-mini-itx.html',
-    'https://n1g.cl/Home/computacion/2082-lian-li-o11-dynamic-mini-white-atx-micro-atx-mini-itx-mini-o11d-mini-s-sfx-sfx-l-.html',
-    'https://n1g.cl/Home/computacion/2208-asus-tuf-gaming-gt502-black-atx-mid-tower-panel-rgb-button-usb-32-type-c-and-2x-usb-30-360mm-280mm-radiator.html',
-    'https://n1g.cl/Home/computacion/1399-chenbro-gabinete-rackeable-4u-server-rm42300-f-12-mm-sgcc-3-bahias-de-525-externas-dvd-opcion.html',
-    'https://n1g.cl/Home/computacion/1614-inwin-a1-plus-pink-mini-itx-tower-con-argb-650w-gold-psu-qi-wireless-charger-case.html',
-    'https://n1g.cl/Home/computacion/2198-lian-li-o11-dynamic-evo-grey-o11deg-aluminum-steel-tempered-glass-atx-mid-tower.html',
-    'https://n1g.cl/Home/computacion/2085-lian-li-o11-dynamic-evo-o11dex-white-aluminum-steel-tempered-glass-atx-mid-tower.html',
-    'https://n1g.cl/Home/computacion/2200-lian-li-o11-dynamic-xl-rog-silver-o11dxl-a-tempered-glass-e-atx-atx-full-tower.html',
-    'https://n1g.cl/Home/computacion/1397--lian-li-o11-dynamic-xl-rog-certificado-white-color-temp.html',
-    'https://n1g.cl/Home/computacion/2199-lian-li-o11-dynamic-xl-rog-black-o11dxl-x-tempered-glass-e-atx-atx-full-tower.html',
-    'https://n1g.cl/Home/computacion/1607-asus-rog-z11-mini-itxdtx-mid-tower-pc-gaming-case-3-slot-graphics-gen-2-type-c-two-usb-32.html',
-    'https://n1g.cl/Home/computacion/2086-nzxt-h510i-all-might-edicion-limitada-tower-pc-gaming-case-front-io-usb-type-c-port.html',
-    'https://n1g.cl/Home/computacion/1439-gabinete-asus-strix-helios-rgb-gx601-negro-fan-incluidos-4x-140mm-eatx-atx-matx-itx-vidrio-templado-aluminio.html',
-    'https://n1g.cl/Home/computacion/2006-asus-rog-strix-helios-eva-edition-aluminio-y-vidrio-templado-90dc0020-b30010-atx.html'
+    'https://n1g.cl/Home/computacion/1640-procesador-amd-ryzen-3-pro-4350g-3800-mhz-4-core-radeon-graphics-sam4-oem-sin-caja-incluye-ventilador-aparte-pn-100-100000148mp.html',
+    'https://n1g.cl/Home/computacion/1807-procesador-amd-ryzen-5-5500-42ghz-turbo-6core12thread-am4-box-100-100000457box.html',
+    'https://n1g.cl/Home/computacion/1647-oem-amd-ryzen-5-pro-4650g-graficos-rx-vega-7-procesador-am4-.html',
+    'https://n1g.cl/Home/computacion/1310-amd-ryzen-5-5600g-graficos-vega-6-core-12-hilos-39ghz-socket-am4-100-100000252box-cpu-retail.html',
+    'https://n1g.cl/Home/computacion/1673-amd-ryzen-5-5600-6-core-socket-am4-65w-100-100000927box.html',
+    'https://n1g.cl/Home/computacion/831-amd-ryzen-5-5600x-6-core-37-ghz-socket-am4-65w-100-100000065box-desktop-processor.html',
+    'https://n1g.cl/Home/computacion/1672-amd-ryzen-7-5700x-ryzen-7-5000-series-8-core-socket-am4-65w-desktop-processor-100-100000926wof.html',
+    'https://n1g.cl/Home/computacion/1311-amd-ryzen-7-5700g-radeon-vega-8-core-38ghz-max-boost-46ghz-socket-am4-graphics.html',
+    'https://n1g.cl/Home/computacion/805-amd-ryzen-7-5800x-47ghz-turbo-8core-16-hilos-105w-tdp-pci-e-40.html',
+    'https://n1g.cl/Home/computacion/1927-amd-ryzen-5-7600x-socket-am5-6-core-12-hilos-max-53ghz-.html',
+    'https://n1g.cl/Home/computacion/1928-amd-ryzen-7-7700x-socket-am5-8-core-16-hilos-max-54ghz-video-int.html',
+    'https://n1g.cl/Home/computacion/1778-amd-ryzen-7-5800x3d-8-core-34-ghz-socket-am4-105w-100-100000651wof.html',
+    'https://n1g.cl/Home/computacion/2274-amd-ryzen-9-7800x3d-8-core-42-ghz-7000-series-am5-120w-amd-radeon-graphics-desktop-100-100000910wof.html',
+    'https://n1g.cl/Home/computacion/806-amd-ryzen-9-5950x-49ghz-turbo-16core-32-hilos-105w-tdp-pci-e-40.html',
+    'https://n1g.cl/Home/computacion/2248-amd-ryzen-9-7900x3d-12-core-44-ghz-socket-am5-120w-amd-radeon-graphics-desktop-processor-100-100000909wof.html',
+    'https://n1g.cl/Home/computacion/1930-amd-ryzen-9-7950x-socket-am5-16-core-32-hilos-max-57ghz-video-int.html',
+    'https://n1g.cl/Home/computacion/2221-amd-ryzen-9-7950x3d-16-core-42-ghz-7000-series-am5-120w-amd-radeon-graphics-desktop-processor-100-100000908wof.html',
+    'https://n1g.cl/Home/computacion/2236-amd-ryzen-threadripper-pro-5955wx-16-core-40-ghz-chagall-pro-socket-swrx8-280w-100-100000447wof.html',
+    'https://n1g.cl/Home/computacion/2009-amd-ryzen-threadripper-pro-5965wx-48-hilos-24-core-38-ghz-socket-swrx8-280w-100-100000446wof.html',
+    'https://n1g.cl/Home/computacion/1917-amd-ryzen-threadripper-pro-5975wx-zen-3-64-hilos-32-core-36-ghz-socket-swrx8-280w-desktop-processor-100-100000445wof.html',
+    'https://n1g.cl/Home/computacion/2008-amd-ryzen-threadripper-pro-5995wx-128-hilos-64-core-27-ghz-socket-swrx8-280w-100-100000444wof.html'
         ]
 
 i = 0
-data2 = []
 def scrap(paginas):
-    for i in range(0, 32,+1):
+    for i in range(0, 21,+1):
         url = paginas[i]
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
@@ -50,8 +38,8 @@ def scrap(paginas):
         stock = soup.find('div', class_='si-items')
         data = list()
         data.append(name.text)
-        data.append(valor.text)
         data.append(stock.text)
+        data.append(valor.text)
         print(data)
         
     return data
--- main2.py
+++ main2.py
@@ -5,28 +5,19 @@ import pandas as pd
 
 paginas= []
 
-{'https://n1g.cl/Home/computacion/1683-gigabyte-gt-1030-low-profile-2gb-pci-express-16x-30-gv-n1030d5-2gl-.html',
-        'https://n1g.cl/Home/computacion/1007-evga-xr1-capturadora-certificada-obs-usb-30-4k-pass-argb-mesclador-de-audio-pc-ps5-xbox-nintendo.html',
-        'https://n1g.cl/Home/computacion/942-asus-tuf-gtx-1650-4gb-128-bit-gddr6-geforce-tuf-gtx1650-o4gd6-p-gaming-pci-express-30.html',
-        'https://n1g.cl/Home/computacion/2099-asrock-amd-radeon-rx-6600-challenger-d-8gb-3x-displayport.html'
-}
 
-url = 'https://n1g.cl/Home/computacion/1683-gigabyte-gt-1030-low-profile-2gb-pci-express-16x-30-gv-n1030d5-2gl-.html'
+url = 'https://n1g.cl/Home/71-amd-cpu'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-#Data
 
-extract = soup.find_all('h1', class_='product_name')
+#extract = soup.find_all('a', )
+extract = soup.find_all('a', href=True)
 
 data = list()
 
 count = 0
-for i in extract:
-    if count < 20:
-        data.append(i.text)
-    else:
-        break
-    count += 1
+for a_href in extract:
+    print(a_href['href'])
 
-print(data)
+
